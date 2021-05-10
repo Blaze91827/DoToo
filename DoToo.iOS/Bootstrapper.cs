5288ca9966d41f8e23797862e64090825aa250bb
Blaze91827 <58652344+Blaze91827@users.noreply.github.com>
2021-05-10 17:28:23 -0400

    Add files via upload

---
+++ DoToo.iOS/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using DoToo;
+
+namespace DoToo.iOS
+{
+    public class Bootstrapper : DoToo.Bootstrapper
+    {
+        public static void Init()
+        {
+            var instance = new Bootstrapper();
+        }
+    }
+}
